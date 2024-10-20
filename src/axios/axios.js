21be581e7387675eb6ad5dfd7ccc8f5152ddbeea
sheryar <sheryarsatti6@gmail.com>
2024-10-20 19:42:03 +0500

    fixed limit

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const apiInstance=axios.create({
     baseURL:'https://vivify-backend-2e2a6dca838b.herokuapp.com/api/v1/',
     // baseURL:'http://127.0.0.1:8000/api/v1/',
-    timeout:50000,
+    timeout:100000,
     headers:{
         'Content-Type':'application/json',
         Accept:'application/json',
